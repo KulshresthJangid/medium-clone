c3ca572506d15e38bc70d8ee907150c4f4a59762
Kulshresth Jangid <kulshresthjangid@gmail.com>
2023-12-06 01:11:35 +0530

    Deleted backend

--- back-end/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.MONGO_URL, {useUnifiedTopology: true, useNewUrlParser: true}).then(() => {
-    console.log('db server connected')
-}).catch((e) => {
-    console.log('error while connecting to db', e)
-})
-
--- back-end/routes/homeRoute.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const router=express.Router()
-const homeController=require('../controllers/homeController')
-
-router.get('/', homeController.home)
-
-module.exports=router
